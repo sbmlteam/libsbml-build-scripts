2e2e0b97ddc2638cb29d02fe2d15c054c80cbdfa
Frank Bergmann <frank@rule.caltech.edu>
2017-08-29 05:39:48 -0700

    scripts for rule

--- rule/create_macos_builds/run_build.sh
+++ rule/create_macos_builds/run_build.sh
@@ -3,9 +3,9 @@
 
 DIRECTORY=$(cd `dirname $0` && pwd)
 
-SOURCE_DIR=$DIRECTORY/../Development/libsbml
+SOURCE_DIR=$DIRECTORY/../../../../Development/libsbml
 
-BUILD_DIR=$DIRECTORY/../Development/build_stable
+BUILD_DIR=$DIRECTORY/../../../../Development/build_stable
 
 
 cd $SOURCE_DIR
--- rule/create_macos_builds/run_build_experimental.sh
+++ rule/create_macos_builds/run_build_experimental.sh
@@ -3,9 +3,9 @@
 
 DIRECTORY=$(cd `dirname $0` && pwd)
 
-SOURCE_DIR=$DIRECTORY/../Development/libsbml-experimental
+SOURCE_DIR=$DIRECTORY/../../../../Development/libsbml-experimental
 
-BUILD_DIR=$DIRECTORY/../Development/build_experimental
+BUILD_DIR=$DIRECTORY/../../../../Development/build_experimental
 
 
 cd $SOURCE_DIR
@@ -23,8 +23,8 @@ fi
 
 cd $BUILD_DIR
 # copy files needed
-cp ${DIRECTORY}/stable/CMakeCache.txt ${BUILD_DIR}/CMakeCache.txt
-cp ${DIRECTORY}/stable/Makefile ${BUILD_DIR}/Makefile
+cp ${DIRECTORY}/experimental/CMakeCache.txt ${BUILD_DIR}/CMakeCache.txt
+cp ${DIRECTORY}/experimental/Makefile ${BUILD_DIR}/Makefile
 
 make rebuild_cache
 make
