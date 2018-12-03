1053287d048e5bf424c26880baf01e7971656232
bgoli <brettolivier@gmail.com>
2018-12-03 03:38:51 -0800

    modified testing routine

---
+++ delay/buildtool/copy_to_surfdrive.bat
@@ -0,0 +1,11 @@
+@echo off
+SET THIS_DIR=%~dp0
+pushd %THIS_DIR%
+
+copy /y C:\inetpub\wwwroot\Files\test_log32.txt C:\Users\brett\Dropbox\sbml\sbmlteam-libsbml-dist\delay_build_logs
+copy /y C:\inetpub\wwwroot\Files\test_log32p.txt C:\Users\brett\Dropbox\sbml\sbmlteam-libsbml-dist\delay_build_logs
+copy /y C:\inetpub\wwwroot\Files\test_log64.txt C:\Users\brett\Dropbox\sbml\sbmlteam-libsbml-dist\delay_build_logs
+copy /y C:\inetpub\wwwroot\Files\test_log64p.txt C:\Users\brett\Dropbox\sbml\sbmlteam-libsbml-dist\delay_build_logs
+copy /y C:\inetpub\wwwroot\Files\build_log.txt C:\Users\brett\Dropbox\sbml\sbmlteam-libsbml-dist\delay_build_logs
+copy /y C:\inetpub\wwwroot\Files\overview.txt C:\Users\brett\Dropbox\sbml\sbmlteam-libsbml-dist\delay_build_logs
+
--- delay/buildtool/run_build.bat
+++ delay/buildtool/run_build.bat
@@ -8,6 +8,8 @@ cd /d %BUILDTOOL_DIR%
 
 call deleteBindings.bat
 
+echo Starting new build: %date% %time%
+
 echo Starting new Build (update): %date% %time% > build_log.txt
 %BUILDTOOL_DIR%\LibSBMLBuild.exe -f %BUILDTOOL_DIR%\libsbml.setting -u >> build_log.txt
 
@@ -17,27 +19,37 @@ echo Starting new Build (32 bit build): %date% %time% >> build_log.txt
 echo Starting new Build (64 bit build): %date% %time% >> build_log.txt
 %BUILDTOOL_DIR%\LibSBMLBuild.exe -f %BUILDTOOL_DIR%\libsbml.setting -64 >> build_log.txt
 
-echo Starting new Build (Python builds): %date% %time% >> build_log.txt
-%BUILDTOOL_DIR%\LibSBMLBuild.exe -f %BUILDTOOL_DIR%\libsbml.setting -py >> build_log.txt
-
 echo Starting new Build (32 bit package build): %date% %time% >> build_log.txt
 %BUILDTOOL_DIR%\LibSBMLBuild.exe -f %BUILDTOOL_DIR%\libsbml.setting -p32 >> build_log.txt
 
 echo Starting new Build (64 bit package build): %date% %time% >> build_log.txt
 %BUILDTOOL_DIR%\LibSBMLBuild.exe -f %BUILDTOOL_DIR%\libsbml.setting -p64 >> build_log.txt
 
+echo Starting new Python Package build: %date% %time%
+
+echo Starting new Build (Python builds): %date% %time% >> build_log.txt
+%BUILDTOOL_DIR%\LibSBMLBuild.exe -f %BUILDTOOL_DIR%\libsbml.setting -py >> build_log.txt
+
 echo Starting new Build (Python package builds): %date% %time% >> build_log.txt
 %BUILDTOOL_DIR%\LibSBMLBuild.exe -f %BUILDTOOL_DIR%\libsbml.setting -ppy >> build_log.txt
+
+echo Starting new installer build: %date% %time%
 
 echo Starting new Build (Installer): %date% %time% >> build_log.txt
 %BUILDTOOL_DIR%\LibSBMLBuild.exe -f %BUILDTOOL_DIR%\libsbml.setting -i >> build_log.txt
 
+echo Starting new MATLAB installer build: %date% %time%
+
 echo Starting new Matlab Packaging: %date% %time% >> build_log.txt
 %BUILDTOOL_DIR%\LibSBMLBuild.exe -f %BUILDTOOL_DIR%\libsbml.setting -pm  >> build_log.txt
+
+echo Starting new R installer build: %date% %time%
 
 cd /d %BUILDTOOL_DIR%
 echo Update R: %date% %time% >> build_log.txt
 call updateR.bat  >> build_log.txt
+
+echo Running tests: %date% %time%
 
 cd /d %BUILDTOOL_DIR%
 echo Running tests: %date% %time% >> build_log.txt
@@ -51,3 +63,5 @@ cd /d %BUILDTOOL_DIR%
 
 call updatelist.bat
 
+echo Completed build and tests: %date% %time%
+call copy_to_surfdrive.bat
