451141bf4b05d698c4870f3513d61f8080271c75
bgoli <brettolivier@gmail.com>
2019-02-28 14:35:31 +0100

    added strict, experimental and cpp_ns support

--- autobuild-option-tests/autobuild.py
+++ autobuild-option-tests/autobuild.py
@@ -6,8 +6,11 @@ from cmakerelib import CMakeReplacements
 rbase = CMakeReplacements()
 
 ## configure test environment
-libsbml_src_path = '/home/sbml/development/sf_libsbml'
+
 base_file = 'ubuntu-1604-ninja-base-CMakeCache.txt' # default for Ubuntu
+
+## configure for experimental
+configure_experimental = False
 
 ## Setup cmake generator
 cmake_generator = 'Ninja' # default for Ubuntu
@@ -15,15 +18,20 @@ cmake_generator = 'Ninja' # default for Ubuntu
 
 # setup option templates
 test_options = [[]] # base only
+test_options = [['strict'], ['cpp_ns']]
 
-## test configurations
-# test_options = [[], ['packages'], ['check'], ['examples'], ['expat'], ['xerces']] # individual options
-# test_options = test_options + [['packages', 'check', 'examples', 'expat']] # all options combined
+## test configurations, all individual options
+# test_options = [[], ['packages'], ['check'], ['examples'], ['expat'], ['xerces'],\
+#  ['strict'], ['cpp_ns']] 
 
 # we insert the base option
 tmp = [opt.insert(0, 'base') for opt in test_options]
 
 print(test_options)
+
+libsbml_src_path = rbase.libsbml_src_path
+if configure_experimental:
+    libsbml_src_path = rbase.libsbml_exp_src_path
 
 if cmake_generator == 'Unix Makefiles':
     cmake_generator_make = rbase.cmake_generator_opts['Unix Makefiles']['cmake_generator_make']
@@ -35,10 +43,16 @@ for conopts in test_options:
     base_split = base_file.split('-')
     new_opts = '-'.join(conopts)
     base_dir = os.path.join(cdir, '-'.join(base_split[:-2]) + '-{}'.format(new_opts), )
-    base_dir = base_dir.replace('-ninja-', '-{}-'.format(cmake_generator.replace(' ', '-').lower()))
+    if configure_experimental:
+        base_dir = base_dir.replace('-ninja-', '-exp-{}-'.format(cmake_generator.replace(' ', '-').lower()))
+    else:
+        base_dir = base_dir.replace('-ninja-', '-{}-'.format(cmake_generator.replace(' ', '-').lower()))
+    rbase.cmake_install_path = base_dir + '-install'
+
     if os.path.exists(base_dir):
         shutil.rmtree(base_dir)
     os.mkdir(base_dir)
+    
     new_cache_file = os.path.join(base_dir, 'CMakeCache.txt')
     new_cache_template = new_cache_file + '.template'
     shutil.copyfile(os.path.join(cdir, base_file), new_cache_template)
@@ -47,11 +61,17 @@ for conopts in test_options:
     Fin = open(new_cache_template, 'r')
     Fout = open(new_cache_file, 'w')
     for l in Fin:
-        # set the cmake build folder
+        # set the cmake build folders
         if cmake_generator == 'Ninja':
-            outlin = l
+            if configure_experimental:
+                outlin = l.replace('-ninja-', '-exp-ninja-')
+            else:
+                outlin = l
         else:
-            outlin = l.replace('-ninja-', '-{}-'.format(cmake_generator.replace(' ', '-').lower()))
+            if configure_experimental:
+                outlin = l.replace('-ninja-', '-exp-{}-'.format(cmake_generator.replace(' ', '-').lower()))
+            else:
+                outlin = l.replace('-ninja-', '-{}-'.format(cmake_generator.replace(' ', '-').lower()))
         # configure cmake generator specific options
         if cmake_generator == "Unix Makefiles":
             if 'CMAKE_GENERATOR:INTERNAL' in outlin:
@@ -65,11 +85,13 @@ for conopts in test_options:
         elif '-base' in l:
             outlin = outlin.replace('/home/sbml/development/autobuild-option-tests', cdir)
             outlin = '{}'.format(outlin.replace('-base', '-{}'.format(new_opts)))
+        # sets the install path
+        if rbase.cmake_install_prefix in l:
+            outlin = outlin.replace('/usr/local', rbase.cmake_install_path)
         # sets the libsbml src path options
         for o_ in rbase.cmake_template_src_paths:
             if o_ in l:
                 outlin = outlin.replace(rbase.cmake_template_src_paths[o_], libsbml_src_path)
-        
         # enable libCheck based unit testing
         if 'check' in conopts:
             if rbase.ubuntu_check_config['disabled'] in outlin:
@@ -96,14 +118,31 @@ for conopts in test_options:
         # additional build options
         # enable packages
         if 'packages' in conopts:
-            for p in rbase.libsbml_packages_disabled:
-                pval = rbase.libsbml_packages_disabled[p]
+            if configure_experimental:
+                pkgs = rbase.libsbml_packages_exp_disabled
+            else:
+                pkgs = rbase.libsbml_packages_disabled
+            for p in pkgs:
+                pval = pkgs[p]
                 if pval in outlin:
                     outlin = outlin.replace(pval, pval.replace('BOOL=OFF', 'BOOL=ON'))
         # enable examples
         if 'examples' in conopts:
             if rbase.libsbml_examples['disabled'] in outlin:
                 outlin = outlin.replace(rbase.libsbml_examples['disabled'], rbase.libsbml_examples['enabled'])
+        # strict includes
+        if 'strict' in conopts:
+            if rbase.libsbml_use_strict_includes['disabled'] in outlin:
+                print(outlin)
+                outlin = outlin.replace(rbase.libsbml_use_strict_includes['disabled'], rbase.libsbml_use_strict_includes['enabled'])
+                print(outlin)
+        # cpp namespace
+        if 'cpp_ns' in conopts:
+            if rbase.libsbml_cpp_namespace['disabled'] in outlin:
+                print(outlin)
+                outlin = outlin.replace(rbase.libsbml_cpp_namespace['disabled'], rbase.libsbml_cpp_namespace['enabled'])
+                print(outlin)
+
 
 
         Fout.write(outlin)
--- autobuild-option-tests/cmakerelib.py
+++ autobuild-option-tests/cmakerelib.py
@@ -23,24 +23,34 @@ class CMakeReplacements(object):
 
     """
     package_names = ['COMP', 'FBC', 'GROUPS', 'L3V2EXTENDEDMATH', 'LAYOUT', 'MULTI', 'QUAL', 'RENDER']
+    package_names_exp = package_names + ['ARRAYS', 'DISTRIB','DYN','REQUIREDELEMENTS', 'SPATIAL']
     template_libsbml_source_path = '/home/sbml/development/sf_libsbml'
+    libsbml_src_path = '/home/sbml/development/sf_libsbml'
+    libsbml_exp_src_path = '/home/sbml/development/sf_libsbml_experimental'
 
     cmake_generator_opts = {'Unix Makefiles' : {'cmake_generator_make' : '/usr/bin/make'}}
     cmake_src_config = ['LIBSBML_ROOT_SOURCE_DIR:PATH=', 'libsbml_SOURCE_DIR:STATIC=',\
         'CMAKE_HOME_DIRECTORY:INTERNAL=']
     
-    cmake_template_src_paths = None
     libsbml_packages_enabled = None
     libsbml_packages_disabled = None
+    libsbml_packages_exp_enabled = None
+    libsbml_packages_exp_disabled = None
+    cmake_template_src_paths = None
+    cmake_install_path = None
+
 
     def __init__(self):
         self.cmake_template_src_paths = {p : self.template_libsbml_source_path for p in self.cmake_src_config}
         self.libsbml_packages_enabled =  {p : 'ENABLE_{}:BOOL=ON'.format(p) for p in self.package_names}
         self.libsbml_packages_disabled =  {p : 'ENABLE_{}:BOOL=OFF'.format(p) for p in self.package_names if p != 'L3V2EXTENDEDMATH'}
-    
+        self.libsbml_packages_exp_enabled =  {p : 'ENABLE_{}:BOOL=ON'.format(p) for p in self.package_names_exp}
+        self.libsbml_packages_exp_disabled =  {p : 'ENABLE_{}:BOOL=OFF'.format(p) for p in self.package_names_exp if p != 'L3V2EXTENDEDMATH'}
+
+
     ubuntu_check_config = {'disabled' : 'WITH_CHECK:BOOL=OFF',
-                           'enabled' : 'WITH_CHECK:BOOL=ON'
-                           }
+                           'enabled' : 'WITH_CHECK:BOOL=ON'}
+                           
     libsbml_xml_parsers = {'expat' : {'enabled' : 'WITH_EXPAT:BOOL=ON',
                                       'disabled' : 'WITH_EXPAT:BOOL=OFF'
                                       },
@@ -49,12 +59,19 @@ class CMakeReplacements(object):
                                       },
                            'xerces' : {'enabled' :  'WITH_XERCES:BOOL=ON',
                                        'disabled' : 'WITH_XERCES:BOOL=OFF'
-                                      }
-                          }
+                                      }}
+
     libsbml_examples = {'enabled' : 'WITH_EXAMPLES:BOOL=ON',
                          'disabled' : 'WITH_EXAMPLES:BOOL=OFF'}
-                          
-
-
     
+    cmake_install_prefix = 'CMAKE_INSTALL_PREFIX:PATH=/usr/local'
+                          
+    libsbml_use_strict_includes = {'enabled' : 'LIBSBML_USE_STRICT_INCLUDES:BOOL=ON',
+                                   'disabled' : 'LIBSBML_USE_STRICT_INCLUDES:BOOL=OFF'}
+
+    libsbml_cpp_namespace = {'enabled' : 'WITH_CPP_NAMESPACE:BOOL=ON',
+                             'disabled' : 'WITH_CPP_NAMESPACE:BOOL=OFF'}
+    
+
+
 
